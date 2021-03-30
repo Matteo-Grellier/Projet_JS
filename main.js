1b8bb1b4847cdeb50168b337dfb103c5445bf188
Luke859 <71589052+Luke859@users.noreply.github.com>
2021-03-30 11:53:55 +0200

    js

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const api_url = 'https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json'
 
-async function loadData() {
+async const loadData = heroes => {
+
+    var heroes = document.createElement("heroes")
 
     const response = await fetch(api_url);
-    const heroes = await response.json()
+    const heroeDescription = await response.json()
 
-    console.log(heroes)
+    console.log(heroeDescription)
+
+    heroes.textContent = heroeDescription
+    
+    body.appendChild(heroes)
 }
 
 loadData();
