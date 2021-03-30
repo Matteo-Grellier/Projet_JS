b334057afbcf0a82869fd419a2751c10fc14b476
MGRELLIER <matteo.grellier@ynov.com>
2021-03-30 15:07:11 +0200

    Move main.js into /js folder

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-< !DOCTYPE html >
+const api_url = 'https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json'
 
-    <html lang="en">
-        <head>
-            <meta charset="utf-8" />
-            <title></title>
-        </head>
-        <body>
+async const loadData = heroes => {
 
-        </body>
-    </html>
+    var heroes = document.createElement("heroes")
+
+    const response = await fetch(api_url);
+    const heroeDescription = await response.json()
+
+    console.log(heroeDescription)
+
+    heroes.textContent = heroeDescription
+    
+    body.appendChild(heroes)
+}
+
+loadData();
