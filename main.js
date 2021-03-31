4bdd54a2eff1d15d48b992ce0d626d17829cf25d
nsegur66 <nico.segura44@gmail.com>
2021-03-31 11:33:01 +0200

    .\main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+export function display() {
+    fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            appendData(data);
+        })
+        .catch(function (err) {
+            console.log('error: ' + err);
+        });
+
+    function appendData(heroes) {
+        var mainContainer = document.getElementById("myData")
+        for (var i = 0; i < heroes.length; i++) {
+            var div = document.createElement("div");
+            div.innerHTML = 'Name: ' + heroes[i].name;
+            mainContainer.appendChild(div);
+        }
+    }
+}
