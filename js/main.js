9e1cc842f013177f91373b138b170497d1294944
nsegur66 <nico.segura44@gmail.com>
2021-04-01 11:23:02 +0200

    .\index.html

--- js/main.js
+++ js/main.js
@@ -6,16 +6,32 @@ export function display() {
         .then(function (data) {
             appendData(data);
         })
+        
         .catch(function (err) {
             console.log('error: ' + err);
         });
-
+        
     function appendData(heroes) {
         var mainContainer = document.getElementById("myData")
         for (var i = 0; i < heroes.length; i++) {
             var div = document.createElement("div");
             div.innerHTML = 'Name: ' + heroes[i].name;
             mainContainer.appendChild(div);
+
+            var race = document.createElement("race");
+            race.innerHTML = 'Race: ' + heroes[i].appearance.race;
+            mainContainer.appendChild(race);
+
+            var powerStats = document.createElement("powerStats");
+            powerStats.innerHTML = 'PowerStats: ' + heroes[i].powerStats;
+            mainContainer.appendChild(powerStats);
+            
+            
         }
     }
+
 }
+
+
+
+
