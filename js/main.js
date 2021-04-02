230ce41f0a40bfd144d02a2743b65493fa44f5a9
yann Bourgeron <75064765+Chakyu23@users.noreply.github.com>
2021-04-02 10:01:02 +0200

    ajout de la barre de recherche

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ export function display() {
         })
         .then(function (data) {
             appendData(data);
+            document.getElementById("srch").onclick = function() {srch(data)};
         })
         .catch(function (err) {
             console.log('error: ' + err);
@@ -22,4 +23,50 @@ export function display() {
             mainContainer.appendChild(race);
         }
     }
+}
+
+export function srch(heroes) {
+    let research = document.getElementById("srchContent").value;
+    let search = new RegExp(research);
+    let finded = []
+    let valid = []
+    console.log(research)
+    for (let i = 0; i < heroes.length; i++) {
+        valid.push(false)
+        for (const property in heroes[i]) {
+
+            if (typeof heroes[i][property] == 'object') {
+
+                for (const subproperty in heroes[i][property]) {
+
+                    if (typeof heroes[i][property][subproperty] == 'object') {
+
+                        for (const sublist in heroes[i][property][subproperty]) {
+
+                            if (search.test(heroes[i][property][subproperty][sublist]) == true) {
+                                valid[i] = true
+
+                            }
+                        }
+                    } else {
+
+                        if (search.test(heroes[i][property][subproperty]) == true) {
+                            valid[i] = true
+
+                        }
+                    }
+                }
+            } else {
+
+                if (search.test(heroes[i][property]) == true) {
+                    valid[i] = true
+
+                }
+            }
+        }
+        if (valid[i] == true) {
+            finded.push(heroes[i])
+        }
+    }
+    console.log(finded)
 }
