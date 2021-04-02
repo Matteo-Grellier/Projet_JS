985c09ab075ccaac9693bad1cf561f155c244482
yann Bourgeron <75064765+Chakyu23@users.noreply.github.com>
2021-04-02 12:12:32 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ export function display() {
         })
         .then(function (data) {
             appendData(data);
+            document.getElementById("srch").onclick = function() {srch(data)};
         })
+    
+
         .catch(function (err) {
             console.log('error: ' + err);
         });
@@ -13,13 +16,82 @@ export function display() {
     function appendData(heroes) {
         var mainContainer = document.getElementById("myData")
         for (var i = 0; i < heroes.length; i++) {
-            var div = document.createElement("div");
+            
+            var tableau = document.getElementById("tab");
+            var row = tableau.insertRow();
+
+            var icon = row.insertCell();
+            var div = row.insertCell();
+            var FullName = row.insertCell();
+            var powerstats = row.insertCell();
+            var appearence = row.insertCell();
+            var placeOfBirth = row.insertCell();
+            var alignment = row.insertCell();
+
+            icon.innerHTML = heroes[i].images.sm;
             div.innerHTML = 'Name: ' + heroes[i].name;
             mainContainer.appendChild(div);
 
             var race = document.createElement("race");
             race.innerHTML = 'Race: ' + heroes[i].appearance.race;
             mainContainer.appendChild(race);
+            FullName.innerHTML = 'Full Name: ' + heroes[i].biography.fullName;
+            powerstats.innerHTML = 'Intelligence: ' + heroes[i].powerstats.intelligence + '/ Strength: ' + heroes[i].powerstats.strength + '/ Speed: ' + heroes[i].powerstats.speed + '/ Durability: ' + heroes[i].powerstats.durability + '/ Power: ' + heroes[i].powerstats.power + '/ Combat: ' + heroes[i].powerstats.combat;
+            appearence.innerHTML = 'Race: ' + heroes[i].appearance.race + ' / Gender: ' + heroes[i].appearance.gender + '\n / Height: ' + heroes[i].appearance.height + ' / Weight: ' + heroes[i].appearance.weight;
+            placeOfBirth.innerHTML = 'Place of Birth: ' + heroes[i].biography.placeOfBirth;
+            alignment.innerHTML = heroes[i].biography.alignment;
+
+
+            mainContainer.appendChild(tableau);
         }
     }
+}
+
+export function srch(heroes) {
+    let research = document.getElementById("srchContent").value;
+    let search = new RegExp(research);
+    let finded = []
+    let valid = []
+    console.log(research)
+    for (let i = 0; i < heroes.length; i++) {
+        valid.push(false)
+        for (const property in heroes[i]) {
+
+            if (typeof heroes[i][property] == 'object') {
+
+                for (const subproperty in heroes[i][property]) {
+
+                    if (typeof heroes[i][property][subproperty] == 'object') {
+
+                        for (const sublist in heroes[i][property][subproperty]) {
+
+                            if (search.test(heroes[i][property][subproperty][sublist]) == true) {
+                                valid[i] = true
+
+                            }
+                        }
+                    } else {
+
+                        if (search.test(heroes[i][property][subproperty]) == true) {
+                            valid[i] = true
+
+                        }
+                    }
+                }
+            } else {
+
+                if (search.test(heroes[i][property]) == true) {
+                    valid[i] = true
+
+                }
+            }
+        }
+        if (valid[i] == true) {
+            finded.push(heroes[i])
+            
+
+        }
+        
+    }
+    console.log(finded)
 }
